269a4ad37b6753cad0586731df5b98a591926814
Edudeiko <evgeniy.dudeyko@gmail.com>
2020-11-06 23:33:31 -0500

    mvp

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,27 @@
+import time
+
+start = time.time()
+
+
 def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = dict()
+
+    for ii in arrays:
+        for jj in ii:
+            if jj not in cache:
+                cache[jj] = 1
+
+            else:
+                cache[jj] += 1
+
+    result = []
+
+    for ii in list(cache.items()):
+        if ii[1] == len(arrays):
+            result.append(ii[0])
 
     return result
 
@@ -15,3 +34,7 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
+end = time.time()
+
+print(f'{end-start}')
