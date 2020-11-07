b64200f40ad04533f1236fc24c544d75dc86be18
Edudeiko <evgeniy.dudeyko@gmail.com>
2020-11-06 22:56:50 -0500

    mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,24 @@
+# from hashtable import HashTable, HashTableEntry
+
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = dict()
+    for idx in range(len(weights)):
+        if weights[idx] in cache and weights[idx] == cache[weights[idx]][1]:
+            return idx, cache[weights[idx]][0]
+
+        else:
+            cache[weights[idx]] = (idx, limit - weights[idx])
+
+    d = list(cache.items())
+    d.sort(reverse=True, key=lambda pair: pair[1][0])
+
+    for ii in d:
+        if ii[1][1] in cache:
+            result = ii[1][0], cache[ii[1][1]][0]
+            return result
 
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
+# from hashtable import HashTable
+
+
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -6,9 +9,37 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # cache = HashTable(length)
+    # route = [None] * length
+
+    # for ii in tickets:
+    #     cache.put(ii.source, ii.destination)
+
+    # find = cache.get("NONE")
+    # idx = 0
+    # curr = find
+    # route[idx] = curr
+    # while curr != "NONE":
+    #     idx += 1
+    #     get = cache.get(curr)
+    #     route[idx] = get
+    #     curr = get
+    # route[idx] = "NONE"
+
+    # return route
+
+    cache = dict()
+
+    for ii in tickets:
+        cache[ii.source] = ii.destination
+
+    route = [cache["NONE"]]
+
+    for idx in range(len(tickets)-2):
+        loop = cache[route[idx]]
+        route.append(loop)
+
+        if idx == len(tickets)-3:
+            route.append("NONE")
 
     return route
